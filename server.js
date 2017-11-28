d2dcc4974b74393b6560ca8f1bf41fdc63442f99
Brayan Santos <v.brayansantos@gmail.com>
2017-11-28 18:15:17 -0200

    add e view phoneBook

--- server.js
+++ server.js
@@ -9,64 +9,33 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
+//templates
+app.set('views', path.join(__dirname, 'public'))
+app.set('view engine', 'ejs')
+
+
+//get list phoneBook
+app.get('/', (request, response) => {
+    var cursor = db.collection('phoneBook').find().toArray(function(err, results){
+        console.log(results);
+        const data = { phoneBooks: results, submitted: false }
+        response.render('index', data)
+    })
+})
 //mogoDb
 MongoClient.connect('mongodb://tulio:123456789@ds119486.mlab.com:19486/phonebook', (err, database) => {
     if (err) return console.log('errrrrrrrrrrrrrroooooooo')
     db = database
     app.listen(PORT, () => {
       console.log('listening on 3000')
-      res.redirect('/')
     })
 })
-//templates
-app.set('views', path.join(__dirname, 'public'))
-app.set('view engine', 'ejs')
 
-app.get('/', (request, response) => {
-    const data = { phoneBook: null, submitted: false }
-    response.render('index', data)
-})
-
+//create phoneBook
 app.post('/createPhoneBook', (request, response) => {
     db.collection('phoneBook').save(request.body, (err, result) => {
         if (err) return console.log(err)
-    
         console.log('saved to database')
-        res.redirect('/')
-      })
-})
-
-
-// app.get('/phoneBook', (request, response) => {
-
-//     // conexao com o banco de dados
-//     phoneBookList = {
-//         first_name: request.query.first_name,
-//         last_name: request.query.last_name,
-//         email: request.query.email,
-//         phone: request.query.phone,
-//         address: request.query.address,
-//         city: request.query.city
-//     }
-//     MongoClient.connect('mongodb://brayansi:12345678@ds119486.mlab.com:19486/phonebook', function (err, db) {
-//         db.collection('Pessoas', function (err, collection) {
-//             collection.insert(this.phoneBookList);
-//             db.collection('Pessoas').count(function (err, count) {
-//                 if (err) throw err;
-//                 console.log('Total Linhas na coleção : ' + count);
-//             });
-//         });
-//     });
-//     console.log(phoneBookList.first_name);
-//     console.log(phoneBookList.last_name);
-//     const data = {
-//         phoneBook: phoneBookList,
-//         submitted: true
-//     }
-
-//     response.render('index', data)
-// })
-
-app.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT)
+        response.redirect('/');
+    })
 })
